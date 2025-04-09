fab4575083073b7acb0db21aff392e0dc25ef6b2
Im23a-demkoa <demkoa@bzz.ch>
2025-04-09 20:37:19 +0200

    2.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,90 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    return (
+        <div>
+            <header>
+                <nav className="flex justify-center">
+                    <div className="fixed top-5 bg-[rgba(173,216,230,0.5)] backdrop-blur-md w-auto p-4 rounded-2xl box-border z-100">
+                        <a href="#1" className="mx-[50px] hover:text-white">HOME</a>
+                        <a href="#2" className="mx-[50px] hover:text-white">ABOUT</a>
+                        <a href="#3" className="mx-[50px] hover:text-white">PROJECTS</a>
+                        <a href="#4" className="mx-[50px] hover:text-white">CONTACT</a>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <section
+                    className="border border-[#D4F0FF] w-full min-h-[700px] bg-[#D4F0FF] shadow-[0_10px_20px_rgba(0,_0,_0,_0.1)] flex flex-col items-center justify-center px-4"
+                    id="1"
+                >
+                    <div className="flex flex-wrap justify-center items-center w-full max-w-[1200px]">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                        <div className="text-4xl md:text-5xl font-bold text-center md:text-left w-full md:w-auto">
+                            ALTIN DEMKU
+                        </div>
+
+                        <div className="mt-4 md:mt-0 md:ml-8">
+                            <img src="/title_image.jpg" alt="hi" className="w-[300px] h-[365px] rounded-[30]" />
+                        </div>
+                    </div>
+                </section>
+
+
+                <div className="flex justify-center mt-[140px] mb-[30px]">
+                    <div className="font-bold text-[#A1C9FF] text-[34px]">About</div>
+                </div>
+                <section className="flex justify-center mb-[140px] px-4" id="2">
+                    <div
+                        className="border border-[#D4F0FF] w-full max-w-screen-lg bg-[#D4F0FF] shadow-2xl flex flex-col md:flex-row items-center md:items-start p-10">
+                        <img src="/about_image.jpg" alt="about_image" className="max-w-[350px] max-h-[454px] " />
+                        <div className="mt-6 md:mt-[10%] md:ml-[3%] text-center md:text-left">
+                            blablablablsdbflasdfjdslfjdslkdfjldsjflkdsjflksjfkdkdkdkdk <br/>
+                            blablablablsdbflasdfjdslfjdslkdfjldsjflkdsjflksjfkdkdkdkdk
+                        </div>
+                    </div>
+                </section>
+
+                <div className="flex justify-center mt-[140px] mb-[30px]">
+                    <div className="font-bold text-[#A1C9FF] text-[34px]">Projects</div>
+                </div>
+                <section className="flex flex-wrap items-center justify-center" id="3">
+                    <div className="rounded-xl shadow-2xl w-[350px] h-[450px] m-[20px] overflow-hidden">
+                        <img src="/FlaskMySQL_picture.jpg" alt="FlaskMySQL " className="h-[225px] w-full"></img>
+                        <div className="h-[225px] bg-amber-50 w-full break-words">
+                            <div className="font-bold text-2xl text-gray-700 pt-[30px] pl-[30px] pr-[30px]">FlaskMySQL
+                            </div>
+                            <div className=" text-xl text-gray-700 pl-[30px] pt-[15px] pb-[10px]">
+                                Flask-Programm mit MySQL zur Verwaltung und Speicherung von Daten.
+                            </div>
+                            <a href="https://github.com/altin-demku/FlaskMySQL" target="_blank"
+                               className="border border-black border-2 p-1 ml-[30px] mt-2 hover:border-[#D4F0FF] hover:text-[#D4F0FF] ">Learn
+                                more.</a>
+                        </div>
+                    </div>
+                    <div className="rounded-xl shadow-2xl w-[350px] h-[450px] m-[20px] overflow-hidden">
+                        <img src="/AutoMailGPT_picture.jpg" alt="AutoMailGPT" className="h-[225px] w-full"></img>
+                        <div className="h-[225px] bg-amber-50 w-full break-words">
+                            <div className="font-bold text-2xl text-gray-700 pt-[30px] pl-[30px] pr-[30px]">AutoMailGPT
+                            </div>
+                            <div className=" text-xl text-gray-700 pl-[30px] pt-[15px] pb-[10px]">
+                                Ein Programm, das E-Mails zusammenfasst und in ein Word-Dokument speichert.
+                            </div>
+                            <a href="https://github.com/altin-demku/AutoMailGPT-" target="_blank"
+                               className="border border-black border-2 p-1 ml-[30px] mt-2 hover:border-[#D4F0FF] hover:text-[#D4F0FF] ">Learn
+                                more.</a>
+                        </div>
+                    </div>
+
+                </section>
+
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+                <div className="border border-[#D4F0FF] w-full max-w-screen-lg bg-[#D4F0FF] flex p-9">
+                    <div>Linked in</div>
+                    <div>Github</div>
+                    <div></div>
+                </div>
+
+            </footer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
